66fae69253f19b897a38aae46ea1243602152cab
Chandan <146570128+chandan-dev-456@users.noreply.github.com>
2026-05-30 11:57:40 +0530

    Update socketManager.js

--- backend/src/controllers/socketManager.js
+++ backend/src/controllers/socketManager.js
@@ -3,7 +3,7 @@ import { Server } from "socket.io";
 export const connectToSocket = (server) => {
     const io = new Server(server, {
         cors: {
-            origin: "http://localhost:5173",
+            origin: process.env.CLIENT_URL,
             credentials: true
         }
     });
